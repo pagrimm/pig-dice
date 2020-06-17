956998516bfce06754f22ecf6348acecc0f7e333
Peter Grimm <pagrimm@gmail.com>
2020-06-17 00:06:45 -0700

    add function to change dice display

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function displayDiceRoll(roll) {
+  let dice = [
+    "<i class=\"fas fa-dice-one\"></i>",
+    "<i class=\"fas fa-dice-two\"></i>",
+    "<i class=\"fas fa-dice-three\"></i>",
+    "<i class=\"fas fa-dice-four\"></i>",
+    "<i class=\"fas fa-dice-five\"></i>",
+    "<i class=\"fas fa-dice-six\"></i>",
+  ];
+  $("#dice-container").html(dice[roll - 1]);
+}
